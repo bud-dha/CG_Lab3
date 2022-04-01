2b5f1a951238a7ff93a946fbfcb0802bc230d141
Nikita Martsenkovsky <martsenkovskynikita@icloud.com>
2022-04-02 02:31:22 +0700

    Добавьте файлы проекта.

---
+++ CG_Lab3/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CG_Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(619, 536);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(637, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(234, 536);
+            this.panel1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Алгоритмы";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 35);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(69, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "ЦДА";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 65);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(99, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Заливка";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 439);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(48, 394);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Цвет";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(48, 484);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 39);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 560);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Pастровые алгоритмы";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ CG_Lab3/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CG_Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int xn, yn, xk, yk; // концы отрезка
+        Bitmap myBitmap; // объект Bitmap для вывода отрезка
+        Color currentColorBorder, currentFillColor; // текущий цвет отрезка и заливки
+        Graphics g;
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+
+            xn = e.X;
+            yn = e.Y;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                myBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                using (g)
+                {
+                    myBitmap = pictureBox1.Image as Bitmap;
+                    CDA(xn, yn, e.X, e.Y);
+                    pictureBox1.Refresh();
+                    pictureBox1.Image = myBitmap;
+                }
+
+                pictureBox1.Image = myBitmap;
+            }
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+        }
+
+        //выполнить
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //отключаем кнопки
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            //создаем новый экземпляр Bitmap размером с элемент
+
+            //на нем выводим попиксельно отрезок
+            myBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromHwnd(pictureBox1.Handle);
+            using (g)
+            {
+                if (radioButton1.Checked == true)
+                {
+                    //рисуем прямоугольник
+                    CDA(10, 10, 10, 110);
+                    CDA(10, 10, 110, 10);
+                    CDA(10, 110, 110, 110);
+                    CDA(110, 10, 110, 110);
+
+                    //рисуем треугольник
+                    CDA(150, 10, 150, 200);
+                    CDA(150, 200, 250, 50);
+                    CDA(150, 10, 250, 175);
+
+                    //рисуем сложную фигуру
+                    /*
+                    CDA(150, 10, 150, 210);
+                    CDA(150, 210, 300, 150);
+                    CDA(300, 150, 225, 110);
+                    CDA(225, 110, 300, 75);
+                    CDA(300, 75, 150, 10);*/
+
+                }
+                else if (radioButton2.Checked == true)
+                {
+
+                    //получаем растр созданного рисунка в mybitmap
+                    myBitmap = pictureBox1.Image as Bitmap;
+
+                    // вызываем рекурсивную процедуру заливки с затравкой
+                    Zaliv(xn, yn);
+
+                }
+                //передаем полученный растр mybitmap в элемент pictureBox
+                pictureBox1.Image = myBitmap;
+                //обновляем pictureBox и активируем кнопки
+                pictureBox1.Refresh();
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                xn = 0;
+                yn = 0;
+                xk = 0;
+                yk = 0;
+            }
+        }
+
+        //цвет
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = colorDialog1.ShowDialog();
+
+            if (dialogResult.Equals(DialogResult.OK) && radioButton1.Checked)
+            {
+                currentColorBorder = colorDialog1.Color;
+            }
+            if (dialogResult.Equals(DialogResult.OK) && radioButton2.Checked)
+            {
+                currentFillColor = colorDialog1.Color;
+            }
+        }
+
+        //очистить
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        private void CDA(int x1, int y1, int x2, int y2)
+        {
+            int i, n;
+            double xt, yt, dx, dy;
+
+            xn = x1;
+            yn = y1;
+            xk = x2;
+            yk = y2;
+            dx = xk - xn;
+            dy = yk - yn;
+            n = 300;
+            xt = xn;
+            yt = yn;
+            for (i = 1; i <= n; i++)
+            {
+                myBitmap.SetPixel((int)xt, (int)yt, currentColorBorder);
+                xt += dx / n;
+                yt += dy / n;
+            }
+        }
+
+        private void Zaliv(int x1, int y1)
+        {
+            Color oldСolor;
+            // получаем цвет текущего пикселя с координатами x1, y1
+            oldСolor = myBitmap.GetPixel(x1, y1);
+            // сравнение цветов происходит в формате RGB
+            // для этого используем метод ToArgb объекта Color
+            if ((oldСolor.ToArgb() != currentColorBorder.ToArgb()) &&
+           (oldСolor.ToArgb() != currentFillColor.ToArgb()))
+            {
+                //перекрашиваем пиксель
+                myBitmap.SetPixel(x1, y1, currentFillColor);
+
+                //вызываем метод для 4-х соседних пикселей
+                Zaliv(x1 + 1, y1);
+                Zaliv(x1 - 1, y1);
+                Zaliv(x1, y1 + 1);
+                Zaliv(x1, y1 - 1);
+            }
+            else
+            {
+                //выходим из метода
+                return;
+            }
+
+        }
+    }
+}
